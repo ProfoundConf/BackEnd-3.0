6a7f44aff73fbd160cd1ffac40dff485f730e0c4
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2025-04-03 20:43:27 +0300

    fix validation

--- Routes/ContactsRoutes.js
+++ Routes/ContactsRoutes.js
@@ -28,6 +28,8 @@ const contactValidation = Joi.object({
 })
 
 const contactOptionalValidation = Joi.object({
+    _id: Joi.objectId().optional().allow(null),
+    __v: Joi.number().optional().allow(null),
     fullName: Joi.string().optional().allow('', null),
     age: Joi.number().optional().allow(null),
     phone: Joi.string().optional().allow('', null),
